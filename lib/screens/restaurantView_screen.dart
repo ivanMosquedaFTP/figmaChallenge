d1dd08801a894773fbcd35653889a06958c9d8d1
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2025-09-20 19:42:50 -0600

    created restaurantViewScreen

---
+++ lib/screens/restaurantView_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RestaurantViewScreen extends StatefulWidget {
+  const RestaurantViewScreen({super.key});
+
+  @override
+  State<RestaurantViewScreen> createState() => _RestaurantViewScreenState();
+}
+
+class _RestaurantViewScreenState extends State<RestaurantViewScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
