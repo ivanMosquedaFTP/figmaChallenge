bf05566882bf44fcc526975dd1b0eedcb0289f79
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2025-09-20 19:42:58 -0600

    created itemDetailsScreen

---
+++ lib/screens/itemDetails_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ItemDetailsScreen extends StatefulWidget {
+  const ItemDetailsScreen({super.key});
+
+  @override
+  State<ItemDetailsScreen> createState() => _ItemDetailsScreenState();
+}
+
+class _ItemDetailsScreenState extends State<ItemDetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
