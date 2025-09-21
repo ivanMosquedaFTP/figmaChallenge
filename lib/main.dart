6a6499790bd6bd1ac4d5367fd4c87669692dbd03
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2025-09-21 16:49:20 -0600

    added temporary routing at main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         "/search": (context) => SearchScreen(),
       },
       // home: LoginScreen(),
-      home: ItemDetailsScreen(),
+      home: SearchScreen(),
     );
   }
 }
